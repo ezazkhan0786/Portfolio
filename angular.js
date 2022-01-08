93d6f0c89f0c8c4a39611b2d25beaae7e61971d6
Ezaz khan <90470272+ezazkhan0786@users.noreply.github.com>
2022-01-08 18:47:06 +0530

    Add files via upload

---
+++ angular.js
@@ -0,0 +1,43 @@
+let app = angular.module("myapp",[]);
+
+app.controller("myctrl", function($scope, $http) {
+    
+    $http.get('https://api.unsplash.com/photos/?client_id=GHweiIULDANw-KNaZs5fAb1htItlqH1I0w9TSfACXKs&per_page=30&page=2')
+        
+let menu = document.querySelector('#menu-bars');
+
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
+
+window.onmousemove = (e) =>{
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
+document.querySelectorAll('a').forEach(links =>{
+
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () =>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+
+});
